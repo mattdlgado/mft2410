f137063f94422c06ac94352410df97663b91ae2d
Mathias <mathias.perez@immune.institute>
2024-09-25 17:45:15 +0200

    Fix container class

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      spacing: {
-        desktop: "1366px",
-        breakpoint: "768px",
-      },
       colors: {
         primary: {
           400: "#f40000",
           500: "#b70000",
           950: "#220202",
         },
+      },
+      container: {
+        center: true,
+        padding: "1rem",
       },
     },
   },
